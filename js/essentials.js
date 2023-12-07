70e43f9fb5a9ff4c38fa9e041218d3e10d9c8d82
cloudkore <cloudkore@outlook.com>
2023-12-07 15:25:36 +0530

    update

--- js/essentials.js
+++ js/essentials.js
@@ -1,5 +1,8 @@
-// main.js
+// essentials.js
 $(function () {
     // Load the navbar dynamically
     $('#navbar-placeholder').load('assets/navbar.html');
+
+    // Load the footer dynamically
+    $('#footer-placeholder').load('assets/footer.html');
 });
